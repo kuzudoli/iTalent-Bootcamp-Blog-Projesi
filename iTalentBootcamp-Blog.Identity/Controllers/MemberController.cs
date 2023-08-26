728aac93b5c93fbaa0ae60c0af4486546b33f529
kuzudoli <enescakkr@windowslive.com>
2023-08-26 23:52:38 +0300

    şifre güncelleme sayfası oluşturuldu

--- iTalentBootcamp-Blog.Identity/Controllers/MemberController.cs
+++ iTalentBootcamp-Blog.Identity/Controllers/MemberController.cs
@@ -1,4 +1,5 @@
 ﻿using iTalentBootcamp_Blog.Identity.Entity;
+using iTalentBootcamp_Blog.Identity.Extensions;
 using iTalentBootcamp_Blog.Identity.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -37,6 +38,42 @@ namespace iTalentBootcamp_Blog.Identity.Controllers
         public async Task SignOut()
         {
             await _signInManager.SignOutAsync();
+        }
+
+        public IActionResult PasswordChange()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PasswordChange(PasswordChangeViewModel request)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            var checkOldPassword = await _userManager.CheckPasswordAsync(user, request.OldPassword);
+
+            if (!checkOldPassword)
+            {
+                ModelState.AddModelIdentityError(new List<string>() { "Eski şifreniz yanlış" });
+                return View();
+            }
+            var result = await _userManager.ChangePasswordAsync(user, request.OldPassword, request.NewPassword);
+
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelIdentityError(result.Errors.Select(x => x.Description).ToList());
+                return View();
+            }
+
+            await _userManager.UpdateSecurityStampAsync(user);
+            await _signInManager.SignOutAsync();
+            await _signInManager.PasswordSignInAsync(user, request.NewPassword, true, false);
+
+            TempData["SuccessMessage"] = "Şifreniz başarıyla değiştirilmiştir.";
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
---
+++ iTalentBootcamp-Blog.Identity/ViewModels/PasswordChangeViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace iTalentBootcamp_Blog.Identity.ViewModels
+{
+    public class PasswordChangeViewModel
+    {
+        [Required(ErrorMessage = "Eski Şifre boş bırakılamaz!")]
+        public string OldPassword { get; set; }
+
+        [Required(ErrorMessage = "Yeni Şifre boş bırakılamaz!")]
+        public string NewPassword { get; set; }
+
+        [Compare(nameof(NewPassword), ErrorMessage = "Şifreler aynı değil!")]
+        [Required(ErrorMessage = "Yeni Şifre Tekrar boş bırakılamaz!")]
+        public string NewPasswordConfirm { get; set; }
+    }
+}
