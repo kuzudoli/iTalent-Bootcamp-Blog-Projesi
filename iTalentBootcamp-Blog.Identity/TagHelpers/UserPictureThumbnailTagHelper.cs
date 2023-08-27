8aa409569fee7c3b3856b38e414a0b65039e10c8
kuzudoli <enescakkr@windowslive.com>
2023-08-27 10:53:12 +0300

    profil resmi göstermek için tag helper yazıldı

---
+++ iTalentBootcamp-Blog.Identity/TagHelpers/UserPictureThumbnailTagHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace iTalentBootcamp_Blog.Identity.TagHelpers
+{
+    public class UserPictureThumbnailTagHelper : TagHelper
+    {
+        public string pictureUrl { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var path = string.IsNullOrEmpty(pictureUrl) ? "/user-images/default-user.jpg" : $"/user-images/{pictureUrl}";
+            
+            output.TagName = "img";
+            output.Attributes.SetAttribute("src", path);
+        }
+    }
+}
