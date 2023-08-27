6158926ad8bdd5e52e9459c241a3f4674a68188f
kuzudoli <enescakkr@windowslive.com>
2023-08-27 09:35:57 +0300

    default route revised

--- iTalentBootcamp-Blog.Identity/Program.cs
+++ iTalentBootcamp-Blog.Identity/Program.cs
@@ -49,6 +49,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Member}/{action=Index}/{id?}");
 
 app.Run();
